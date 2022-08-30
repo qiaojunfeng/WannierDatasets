16a2d452df2611aa4b32144683eb85caec27208b
Junfeng Qiao <qiaojunfeng@outlook.com>
2022-08-30 10:14:03 +0200

    Add SSSP pseudo

---
+++ pseudo/download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# pseudos are downloaded from SSSP
+# https://www.materialscloud.org/discover/sssp/table/efficiency
+
+# auto download with either aiida-pseudo, or wget
+# aiida-pseudo install sssp -x PBEsol --download-only
+
+F='SSSP_1.1.2_PBEsol_efficiency.tar.gz'
+wget "https://archive.materialscloud.org/record/file?filename=${F}&parent_id=19" -O $F
+tar xvf $F
--- tutorials/6-split-MoS2/pseudo/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# pseudo can be downloaded from SSSP
-#    https://www.materialscloud.org/discover/sssp/table/efficiency
