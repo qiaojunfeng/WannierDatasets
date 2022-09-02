41512a66095641ff242bd51fd2f8611aab731b12
Junfeng Qiao <qiaojunfeng@outlook.com>
2022-09-02 17:39:57 +0200

    Add 5-parallel_transport

--- tutorials/5-parallel_transport/run.sh
+++ tutorials/5-parallel_transport/run.sh
@@ -8,13 +8,15 @@ NK=8
 PWX=pw.x
 W90X=wannier90.x
 P2WX=pw2wannier90.x
-WJL=$HOME/.julia/bin/wannier
 
 F='scf'
 mpirun -n $NP $PWX -nk $NK -in $F.in > $F.out
 
-F='bands'
-mpirun -n $NP $PWX -nk $NK -in $F.in > $F.out
+#F='bands'
+#mpirun -n $NP $PWX -nk $NK -in $F.in > $F.out
+#
+#F='proj'
+#mpirun -n $NP projwfc.x -nk $NK -in $F.in > $F.out
 
 F='nscf'
 mpirun -n $NP $PWX -nk $NK -in $F.in > $F.out
@@ -28,16 +30,3 @@ mpirun -n $NP $P2WX -in $F.in > $F.out
 F='mos2.win'
 mpirun -n $NP $W90X $F
 
-../split.sh --rotate-unk --nval 9    mos2 > split.out
-
-cd val/
-$WJL optrot mos2 > optrot.out
-F='mos2.win'
-mpirun -n $NP $W90X $F
-cd ../
-
-cd cond/
-$WJL optrot mos2 > optrot.out
-F='mos2.win'
-mpirun -n $NP $W90X $F
-cd ../
